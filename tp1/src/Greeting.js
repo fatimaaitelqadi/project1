7d0475a274dd2f723e84ca6dde7e6ee12e34fe86
fatimaaitelqadi <fatimaaitelqadi58@gmail.com>
2024-11-18 14:29:05 +0100

    Modification: mise à jour du text

--- tp1/src/Greeting.js
+++ tp1/src/Greeting.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Greeting(props) {
   return (
     <div>
-      <h1>bonjour {props.name}</h1>
+      <h1>hello {props.name}</h1>
     </div>
   )
 }
